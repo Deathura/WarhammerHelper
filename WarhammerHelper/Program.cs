e82cbba4db6280cf68d763c4a3442c696af69353
Deathura <sebastien.altounian@gmail.com>
2020-04-15 17:21:41 +0200

    Create Team Class

--- WarhammerHelper/Program.cs
+++ WarhammerHelper/Program.cs
@@ -19,4 +19,21 @@ namespace WarhammerHelper
             Application.Run(new Form1());
         }
     }
+
+    public class Team
+    {
+        public String TeamName = "TeamName";
+        public Int16 nbUnit = 0;
+
+        private void AddUnit()
+        {
+            nbUnit += 1;
+        }
+
+        private void ChangeTeamName(string NewName)
+        {
+            TeamName = NewName;
+            //TODO Secure entry (check if its really a correct string)
+        }
+    }
 }
